60ba2f290818a8f19041a5162b4e0f06365ac83e
Ipsha Ghimire <ipshagh2000@gmail.com>
2022-05-23 20:21:19 +0545

    button click function added

---
+++ index.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById('btn');
+
+const btnclick=()=>{
+
+const myname=prompt('enter your name');
+console.log(myname);
+}
+btn.addEventListener('click',btnclick);
+
+
+
+
+
